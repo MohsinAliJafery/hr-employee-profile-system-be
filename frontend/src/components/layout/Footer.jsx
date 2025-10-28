16adc3ce4d0cb7110b6869271dacb6cf2d559c93
MohsinAliJafery <70712589+MohsinAliJafery@users.noreply.github.com>
2025-10-28 12:03:03 +0500

    Delete frontend directory

--- frontend/src/components/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="bg-gray-200 text-center py-2 text-sm text-gray-700">
-      © {new Date().getFullYear()} EPR Management System — All Rights Reserved
-    </footer>
-  );
-};
-
-export default Footer;
